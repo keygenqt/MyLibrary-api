74972ab03e70bf7a3f93abdfa0be1d875eebcea1
keygenqt <keygenqt@gmail.com>
2020-11-17 11:15:58 +0300

    update ErrorResponse

--- src/main/kotlin/com/keygenqt/mylibrary/base/error/ErrorResponse.kt
+++ src/main/kotlin/com/keygenqt/mylibrary/base/error/ErrorResponse.kt
@@ -7,7 +7,7 @@ import java.time.*
 
 class ErrorResponse(
     @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
-    val time: LocalDateTime,
+    val datetime: LocalDateTime,
     val status: Int,
     val error: HttpStatus,
     val message: String
