3f44dfa1f16fccaa059a44463824cb2eb186a387
keygenqt <keygenqt@gmail.com>
2020-12-09 10:58:57 +0300

    add notification

---
+++ src/main/resources/db/migration/V1_0_3__create_notifications.sql
@@ -0,0 +1,10 @@
+create table notifications
+(
+    id              int                                         not null auto_increment,
+    user_id         int,
+    notification    TEXT                                        not null,
+    type            enum ('direct', 'common')                   not null,
+    status          enum ('open', 'done', 'pending', 'error')   not null,
+    created_at      TIMESTAMP                                   not null default now(),
+    primary key (id)
+);
---
+++ src/main/resources/db/migration/V1_0_7__insert_notification_table.sql
@@ -0,0 +1,29 @@
+insert into notifications (user_id, notification, type, status)
+values (2,
+        'Notification text for user 1',
+        'direct',
+        'open');
+
+insert into notifications (user_id, notification, type, status)
+values (null,
+        'Notification text for users common',
+        'common',
+        'open');
+
+insert into notifications (user_id, notification, type, status)
+values (2,
+        'Notification text for user 3',
+        'direct',
+        'open');
+
+insert into notifications (user_id, notification, type, status)
+values (null,
+        'Notification text for user 4',
+        'direct',
+        'open');
+
+insert into notifications (user_id, notification, type, status)
+values (2,
+        'Notification text for user 5',
+        'direct',
+        'open');
