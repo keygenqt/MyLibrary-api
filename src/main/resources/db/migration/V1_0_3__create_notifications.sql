f695ae52bbb5440c3185ee150fa81a99f905231f
keygenqt <keygenqt@gmail.com>
2020-12-12 13:06:18 +0300

    notifications uri

--- src/main/resources/db/migration/V1_0_3__create_notifications.sql
+++ src/main/resources/db/migration/V1_0_3__create_notifications.sql
@@ -4,6 +4,7 @@ create table notifications
     user_id      int                                       not null,
     channel_id   varchar(250)                              not null default 'channel_server',
     title        varchar(250)                              not null default 'MyLibrary',
+    uri          varchar(500)                              not null,
     body         TEXT                                      not null,
     status       enum ('open', 'done', 'pending', 'error') not null,
     created_at   TIMESTAMP                                 not null default now(),
--- src/main/resources/db/migration/V1_0_7__insert_notification_table.sql
+++ src/main/resources/db/migration/V1_0_7__insert_notification_table.sql
@@ -1,24 +1,5 @@
-insert into notifications (user_id, body, status)
+insert into notifications (user_id, uri, body, status)
 values (2,
-        'Notification text for user 1',
-        'open');
-
-insert into notifications (user_id, body, status)
-values (1,
-        'Notification text for users common',
-        'open');
-
-insert into notifications (user_id, body, status)
-values (2,
-        'Notification text for user 3',
-        'open');
-
-insert into notifications (user_id, body, status)
-values (1,
-        'Notification text for user 4',
-        'open');
-
-insert into notifications (user_id, body, status)
-values (2,
-        'Notification text for user 5',
+        'http://192.168.1.68:8080/books/1',
+        'Notification text',
         'open');
