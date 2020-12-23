73ec82a70f277dac679a4a70a2effd386e329b8e
keygenqt <keygenqt@gmail.com>
2020-12-23 22:09:29 +0300

    move to java

---
+++ src/main/java/com/keygenqt/mylibrary/models/assemblers/UserAssembler.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Vitaliy Zarubin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.keygenqt.mylibrary.models.assemblers;
+
+import com.keygenqt.mylibrary.models.User;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.LinkRelationProvider;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.OneToOne;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+@Component
+public class UserAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
+
+    private final LinkRelationProvider relProvider;
+    private final EntityLinks entityLinks;
+
+    @Autowired
+    public UserAssembler(LinkRelationProvider relProvider, EntityLinks entityLinks) {
+        this.relProvider = relProvider;
+        this.entityLinks = entityLinks;
+    }
+
+    @NotNull
+    @Override
+    public EntityModel<User> toModel(@NotNull User model) {
+        ArrayList<Link> links = new ArrayList<>() {{
+            add(entityLinks.linkToItemResource(User.class, model.getId()).withSelfRel());
+            add(entityLinks.linkToItemResource(User.class, model.getId()).withRel(relProvider.getItemResourceRelFor(User.class)));
+        }};
+        for (Field value : User.class.getDeclaredFields()) {
+            if (value.getAnnotation(OneToOne.class) != null) {
+                links.add(entityLinks.linkToItemResource(User.class, model.getId() + "/" + value.getName()).withRel(value.getName()));
+            }
+        }
+        return EntityModel.of(model, links);
+    }
+}
--- src/main/kotlin/com/keygenqt/mylibrary/models/assemblers/GenreAssembler.kt
+++ src/main/kotlin/com/keygenqt/mylibrary/models/assemblers/GenreAssembler.kt
@@ -1,41 +1,41 @@
-/*
- * Copyright 2020 Vitaliy Zarubin
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.keygenqt.mylibrary.models.assemblers
-
-import com.keygenqt.mylibrary.models.*
-import org.springframework.beans.factory.annotation.*
-import org.springframework.hateoas.*
-import org.springframework.hateoas.server.*
-import org.springframework.stereotype.*
-
-@Component
-internal class GenreAssembler : RepresentationModelAssembler<Genre, EntityModel<Genre>> {
-
-    @Autowired
-    private lateinit var relProvider: LinkRelationProvider
-
-    @Autowired
-    private lateinit var entityLinks: EntityLinks
-
-    override fun toModel(model: Genre): EntityModel<Genre> {
-        return EntityModel.of<Genre>(model,
-            entityLinks.linkToItemResource(Genre::class.java, model.id!!).withSelfRel(),
-            entityLinks.linkToItemResource(Genre::class.java, model.id!!)
-                .withRel(relProvider.getItemResourceRelFor(Genre::class.java))
-        )
-    }
-}
+///*
+// * Copyright 2020 Vitaliy Zarubin
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package com.keygenqt.mylibrary.models.assemblers
+//
+//import com.keygenqt.mylibrary.models.*
+//import org.springframework.beans.factory.annotation.*
+//import org.springframework.hateoas.*
+//import org.springframework.hateoas.server.*
+//import org.springframework.stereotype.*
+//
+//@Component
+//internal class GenreAssembler : RepresentationModelAssembler<Genre, EntityModel<Genre>> {
+//
+//    @Autowired
+//    private lateinit var relProvider: LinkRelationProvider
+//
+//    @Autowired
+//    private lateinit var entityLinks: EntityLinks
+//
+//    override fun toModel(model: Genre): EntityModel<Genre> {
+//        return EntityModel.of<Genre>(model,
+//            entityLinks.linkToItemResource(Genre::class.java, model.id!!).withSelfRel(),
+//            entityLinks.linkToItemResource(Genre::class.java, model.id!!)
+//                .withRel(relProvider.getItemResourceRelFor(Genre::class.java))
+//        )
+//    }
+//}
